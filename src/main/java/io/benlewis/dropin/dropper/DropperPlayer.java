64c26de96a2a2875d4ee570d36c3a144cf486dd5
benlewis9000 <benjaminlewis9000@gmail.com>
2023-01-19 01:38:37 +0000

    Add DropperPlayer and PlayerWrapper

---
+++ src/main/java/io/benlewis/dropin/dropper/DropperPlayer.java
@@ -0,0 +1,12 @@
+package io.benlewis.dropin.dropper;
+
+import io.benlewis.dropin.util.PlayerWrapper;
+import org.bukkit.entity.Player;
+
+public class DropperPlayer extends PlayerWrapper {
+
+    public DropperPlayer(Player player) {
+        super(player);
+    }
+
+}
